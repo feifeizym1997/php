f8f57eff8a8e063cff29a00d01bed739bc0f7dc2
feifeizym1997 <38244503+feifeizym1997@users.noreply.github.com>
2018-04-19 09:55:26 +0800

    Create 7.php

---
+++ 7.php
@@ -0,0 +1,12 @@
+<?php
+echo  __FILE__;
+echo "<br />";
+echo  __LINE__;
+echo "<br />";
+echo PHP_VERSION;
+echo "<br />";
+echo PHP_OS;
+echo "<br />";
+?>
+
+//2,4 是2条下划线   __FILE__ :php程序文件名 __LINE__ :PHP程序文件行数。
